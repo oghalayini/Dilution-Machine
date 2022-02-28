8c3490e80189fde0b89422a5148c0d1fa41c4097
Omar Ghalayini <64828040+oghalayini@users.noreply.github.com>
2022-02-28 16:52:14 +0200

    Add pin definitions, button tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,47 @@
 #include <Arduino.h>
 
+const int dir = 2;
+const int step = 3;
+
+const int colorButton = 8;
+
+const int MOSFET = 9;
+
+const int metalButton = 10;
+const int metalButtonLED = 11;
+
+const int encoderRight = 18;
+const int encoderLeft = 19;
+const int encoderButton = 30;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(dir,OUTPUT);
+  pinMode(step,OUTPUT);
+
+  pinMode(colorButton,INPUT);
+
+  pinMode(MOSFET,OUTPUT);
+
+  pinMode(metalButton,INPUT);
+  pinMode(metalButtonLED,OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  /*
+  Serial.print("Color Button Status: ");
+  Serial.println(digitalRead(colorButton));
+
+  Serial.print("Metal Button Status: ");
+  Serial.println(digitalRead(metalButton));
+
+  if (digitalRead(metalButton)) {
+    digitalWrite(metalButtonLED,HIGH);
+  } else {
+    digitalWrite(metalButtonLED,LOW);
+  }
+  */
+
+  delay(1500);
 }
